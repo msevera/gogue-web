1a44c9d15d13a71c58672866d54d06ce622dd73a
Mike Severa <michael.svr@gmail.com>
2025-08-27 13:57:41 +0200

    Add Facebook Pixel integration for tracking page views and enhance noscript support in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,20 @@ export default function RootLayout({
               s.parentNode.insertBefore(b, s);})(window.lintrk);
           `}
         </Script>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1096444012053015');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -73,6 +87,12 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html:
               '<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=7768642&fmt=gif" />',
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1096444012053015&ev=PageView&noscript=1" />',
           }}
         />
       </body>
